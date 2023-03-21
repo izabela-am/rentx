1d1330d521fb7d86d9a9a83add7f6702f302a847
Rodrigo Rocha <rmaues.rocha@rmaues.com>
2023-03-21 17:47:59 -0400

    Criada função para buscar na API do HIBP uma senha, para saber se ela foi ou não vazada.

--- src/security/passwords/entropy.py
+++ src/security/passwords/entropy.py
@@ -1,20 +1,46 @@
 ''' Function to calculate te entropy  '''
 
+import sys
 import math
+import hashlib
+import requests
 ## import random
 
 
 def entropy(senha):
     tamanho_senha = len(senha)
     ##    tamanho_conjunto = len(set(senha))
-    tamanho_conjunto = 95
+    tamanho_conjunto = 94
     entropia = math.log2(tamanho_conjunto**tamanho_senha)
     if entropia >= 60:
         print(f"O valor de entropia para a senha é {entropia} é adequado.")
         return entropia
+    print(f"O valor {entropia} apresenta um valor menor que o adequado.")
+
+
+## Validando se uma senha ja tem leak
+def hashSenha(senha):
+    sha1Senha = hashlib.sha1(senha.encode('utf-8')).hexdigest().upper()
+    inicio, final = sha1Senha[:5], sha1Senha[-5:]
+    ## Endereco da API
+    url = f'https://api.pwnedpasswords.com/range/{inicio}'
+    res = requests.get(url)
+    ## Validando a resposta
+    if res.status_code != 200:
+        raise RuntimeError(f"Algo deu errado, veja se a API está ok")
+    ## Sepando as linhas
+    lista_hashs = res.text.splitlines()
+
+    # Validando se tem senhas vazadas
+    for hash in lista_hashs:
+        sufixo = hash.split(":")[0]
+        if sufixo[-5:] == final:
+            count = int(hash.split(":")[1])
+            print(f"A senha {senha} foi encontrada {count} vezes")
+            break
     else:
-        print(f"O valor {entropia} apresenta um valor menor que o adequado.")
+        print("Senha ainda sem ser vazada!")
 
 
-senha = input("Digitie uma senha: ")
-entropy(senha)
+senha = input("Qual a senha para validar? ")
+hashSenha(senha)
